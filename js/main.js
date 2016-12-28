9d7f092db34100742b2b0cdb5375e59b53f85386
Evandro Macedo <evandro.hcm@gmail.com>
2016-12-27 23:04:41 -0300

    Creates function to toggle the loop

--- js/main.js
+++ js/main.js
@@ -16,3 +16,41 @@ function playSound(e) {
 // const keys = Array.from(document.querySelectorAll('.key'));
 // keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
+
+//////////////////////////////////////////////////////////
+
+var loopToggleButton = document.getElementById('loop-toggle'),
+    loopSelect       = document.getElementById('loop-select'),
+    loopAudio        = document.getElementById('loop-audio');
+
+function loopToggle(e) {
+  var loopName = loopSelect.value;
+
+  if (!loopName) return;
+
+  if (loopToggleButton.getAttribute("data-status") == "start") {
+    loopAudio.src = "sounds/loop/" + loopName + ".wav";
+    loopAudio.play();
+
+    loopToggleButton.setAttribute("data-status", "pause");
+    loopToggleButton.textContent = "Pause Loop";
+  }
+  else {
+    loopAudio.pause();
+
+    loopToggleButton.setAttribute("data-status", "start");
+    loopToggleButton.textContent = "Start Loop";
+  }
+};
+
+function resetAudio(e) {
+  if (!loopAudio.paused) {
+    loopAudio.pause();
+
+    loopToggleButton.setAttribute("data-status", "start");
+    loopToggleButton.textContent = "Start Loop";
+  }
+};
+
+loopToggleButton.addEventListener('click', loopToggle);
+loopSelect.addEventListener('change', resetAudio);
